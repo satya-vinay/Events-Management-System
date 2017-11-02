c5d4fb3ed982b0a8c09abc462d65de922bf1203a
= <satyavinay2@gmail.com>
2017-11-02 18:20:42 +0530

    delete page

--- report.php
+++ report.php
@@ -2,24 +2,25 @@
 
 <?php include('header.php'); ?>
 
-
+ <div class="container">
 <center>
+
 <h3 style="padding: 15px;">Registration Report</h3>
 
-    <div class="table-responsive table-hover table-bordered col-12 ">
+    <div class="table-responsive table-hover table-bordered col-12 " >
 
-     <table class="table ">
+     <table class="table " >
   <thead > 
     <tr class="table-primary">
            
                 <th width="6%">Name</th>
-                <th width="4%">Program</th>
+                <th width="4%">Event</th>
                 <th width="2%">Email</th>
-                <th width="2%">Year</th>
-                <th width="4%">Gender</th>
-                <th width="7%">From date</th>
-                <th width="7%">To date</th>
-                <th width="2%">No of people</th>
+                <th width="1%">Year</th>
+                <th width="1%">Gender</th>
+                <th width="4%">From date</th>
+                <th width="4%">To date</th>
+                <th width="4%">No of people</th>
             </tr>
 
 
@@ -35,10 +36,12 @@
             </center>
             </form>
         </div>
-
-
-</div>
+        </thead>
+        </table>
+    </div>
+    </div>
 </center>
+</div>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
