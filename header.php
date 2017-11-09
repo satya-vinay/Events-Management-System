a7816ea9f27a8c9b7615dc953d9aeb4c9bde4130
= <satyavinay2@gmail.com>
2017-11-09 22:40:15 +0530

    Title

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 
-	<title></title>
+	<title>KLU Event Registration</title>
 
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
